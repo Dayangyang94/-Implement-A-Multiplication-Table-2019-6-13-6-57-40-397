8c8d8de0eb8b1582272510225c317f00785440f9
gaoyang <2436512082@qq.com>
2019-08-21 19:38:37 +0800

    修改

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,38 +1,36 @@
-const {isValid, generateMultiplyArr,printMultiplyTable} = require('../main');
+const {isvalid,generateMultilplyArray,printMultiplyTable} = require('../main');
+it('should be valid numbers', () =>{
+    //given
+    const first=2;
+    const second=3;
+    //when
+    const result=isvalid(first,second);
+    //then
+    expect(result).toBe(true);
+});
+it('should canculate multiply table',() =>{
+    //given
+    const first=2;
+    const second=3;
+    //when
+    let result=generateMultilplyArray(first,second);
+    //then
+    expect(result[0][0]).toBe("2*2=4");
+    // expect(result[1][0]).toBe("2*3=6");
+    // expect(result[1][1]).toBe("3*3=9");
+
+});
 
 
-//判断有效输入
-    it ('should  numbers be valid', () => {
-        // given
-        var firstNumber=2;
-        var secondNumber=3;
-        //when
-        var result=isValid(firstNumber,secondNumber);
-        //then
-        expect(result).toBe(true);
-        });
-      
-      
-        it ('should  generated array with table items', () => {
-            // given
-            var firstNumber=2;
-            var secondNumber=3;
-            //when
-            var generateArr=generateMultiplyArr(firstNumber,secondNumber);
-            //then
-            expect(generateArr[0][0]).toBe("2*2=4");
-            expect(generateArr[1][0]).toBe("2*3=6");
-            expect(generateArr[1][1]).toBe("3*3=9");
-            });
-            it ('should print multiply table', () => {
-               //given
-              const array = [];
-              array[0] = [];
-              array[0][0] = "2*2=4";
+it ('should generate result string', () => {
+    //given
+    const array = [];
+    array[0] = [];
+    array[0][0] = "2*2=4";
 
-               //when
-              const result = printMultiplyTable(array);
+    //when
+    const result = printMultiplyTable(array);
 
-             //then
-            expect(result).toBe("2*2=4 \n");
-                });
+    //then
+    expect(result).toBe("2*2=4 \n");
+});
--- main.js
+++ main.js
@@ -1,41 +1,48 @@
 
 function createMutiplyTable(first,second){
-    var output="";
-    var valid=isValid(first,second);
+    var valid=isvalid(first,second);
     if(valid){
-        return valid;
-    } 
-    var multiplyArr=generateMultiplyArr(first,second);
-    output =printMultiplyTable(multiplyArr);
-    return output;
-}
-
-function isValid(first,second){
-    return first<second
-}
-function generateMultiplyArr(first,second){
-  var result=[];
-  for(var i=first;i<=second;i++){
-        result[i-first]= []; 
-        for(var j = first;j<=i;j++){
-            result[i-first][j-first] = j+"*"+i+"="+i*j;
-        }
-    
+        result=generateMultilplyArray(first,second);
     }
-  return result;
 }
-function printMultiplyTable(multiplyArr){
-    var output="";
-    for(var i=0;i<multiplyArr.length;i++){
-        for(var j=0;j<multiplyArr[i].length;j++){
-            output=output+multiplyArr[i][j]+" ";
+function isvalid(first,second){
+    return first<second&&first>=1&&first<1000&&second>=1&&second<1000
+}
+    // function generateMultilplyArray(first,second){
+    //    var result=[];
+    //    for(var i=first;i<=second;i++){
+    //         result[i-first]=[];
+    //         for(var j=first;j<=i;j++){
+    //             result[i-first][j-first]=j+"*"+i+j*i;
+    //         }
+    //    } 
+    // }
+    function generateMultilplyArray(first,second){
+        let result=[];
+        let times=second-first+1;
+        for(let rowIndex=0;rowIndex<times;rowIndex++){
+            var row=[];
+            for(columnIndex=0;columnIndex<(rowIndex+1);columnIndex++){
+                var res=(rowIndex+first)*(columnIndex+first);
+                var output=(columnIndex+first)+"*"+(rowIndex+first)+"="+res;
+                row.push(output);
+            }
+            result.push(row);
         }
-        output+="\n";
+        return result;
     }
-    return output;
-}
+    function printMultiplyTable(multiplyArray){ 
+        var output = "";
+        for(var i=0;i<multiplyArray.length;i++){
+            for(j=0;j<multiplyArray[i].length;j++){
+                output = output+multiplyArray[i][j]+" ";
+            }
+            output += "\n";
+        }
+        return output;
+    }
 module.exports = {
-    isValid,
-    generateMultiplyArr,
+    isvalid,
+    generateMultilplyArray,
     printMultiplyTable
 };
